ec0f3081a0a6df13b43ecbd2c70072929551afe9
TomDijkema <tom.dijkema@naturalis.nl>
2023-07-04 10:34:29 +0200

    Add Landing Page
    Commit adds Landing page instead of directly redirecting the user to the Keycloak instance.
    
    Adds:
    - Landing page
    
    Modifies:
    - Keycloak service to not directly link to the login page

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,35 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import KeycloakService from "keycloak/Keycloak";
 
 /* Import Styles */
 import './App.css';
 
 /* Import Components */
 import Home from "components/home/Home";
+import Landing from "components/landing/Landing";
 
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+  /* If user is logged in, show actionable Home page, otherwise show landing page */
+  if (KeycloakService.IsLoggedIn()) {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    );
+  } else {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/landing/Landing.tsx
@@ -0,0 +1,44 @@
+/* Import Components */
+import KeycloakService from 'keycloak/Keycloak';
+import { Container, Row, Col } from 'react-bootstrap';
+
+/* Import Webroot */
+import DiSSCoLogo from 'webroot/img/dissco-logo-web.svg';
+
+
+const Landing = () => {
+    return (
+        <Container className="h-100 ">
+            <Row className="h-100 align-items-center">
+                <Col>
+                    <Row>
+                        <Col className="text-center">
+                            <h1> DiSSCo Orchestration Services </h1>
+                        </Col>
+                    </Row>
+                    <Row className="mt-3">
+                        <Col md={{ span: 2, offset: 5 }}>
+                            <img src={DiSSCoLogo} alt="DiSSCo Logo" />
+                        </Col>
+                    </Row>
+                    <Row className="mt-5">
+                        <Col className="text-center">
+                            <p> Please login to continue </p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="d-flex justify-content-center">
+                            <button type="button" className="primaryButton px-3 py-2"
+                                onClick={() => KeycloakService.Login()}
+                            >
+                                Login to DiSSCo
+                            </button>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Landing;
