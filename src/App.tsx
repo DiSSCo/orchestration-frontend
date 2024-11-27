c1588232d7ad3b47e86e0c3ec982ddd4a3e2576f
TomDijkema <tom.dijkema@naturalis.nl>
2024-11-27 08:37:12 +0100

    Allow application to be seen (read only) for users who are not logged in
    Allow application to be seen (read only) for users who are not logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 /* Import Components */
 import {
   BrowserRouter as Router,
-  Routes,
-  Route
+  Routes
 } from "react-router-dom";
-import KeycloakService from "app/Keycloak";
 
 /* Import Routes */
 import AppRoutes from 'app/Routes';
@@ -12,29 +10,15 @@ import AppRoutes from 'app/Routes';
 /* Import Styles */
 import './App.css';
 
-/* Import Components */
-import Landing from "components/landing/Landing";
-
 
 const App = () => {
-  /* If user is logged in, show actionable Home page, otherwise show landing page */
-  if (KeycloakService.IsLoggedIn()) {
-    return (
-      <Router>
-        <Routes>
-          {AppRoutes}
-        </Routes>
-      </Router>
-    );
-  } else {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-        </Routes>
-      </Router>
-    );
-  }
-}
+  return (
+    <Router>
+      <Routes>
+        {AppRoutes}
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
